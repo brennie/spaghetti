e723ef4d9fb4d167ffa0067a1ce3e2d1095795ec
Barret Rennie <barret@brennie.ca>
2014-05-28 14:48:37 -0600

    Remove package comment from all tt files but tt.go
    This change removes redundant lines from the godoc output of the tt package.

--- tt/domain.go
+++ tt/domain.go
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// The timetabling package.
 package tt
 
 // A domain is a set of rooms and times.
--- tt/rat.go
+++ tt/rat.go
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// The timetabling package.
 package tt
 
 // An assignment of a room and time.
